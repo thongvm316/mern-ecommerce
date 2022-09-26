1d6f4a6253d3b2da49f3864ac90d2db34cdf1048
thongvm <56250318+thongvm316@users.noreply.github.com>
2022-09-26 15:46:38 +0700

    Feature/auth firebase (#1)
    * config firebase FE, BE
    * added auth

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Menu, Badge } from "antd";
+import React, { useState } from 'react'
+import { Menu, Badge } from 'antd'
 import {
   AppstoreOutlined,
   SettingOutlined,
@@ -8,49 +8,49 @@ import {
   LogoutOutlined,
   ShoppingOutlined,
   ShoppingCartOutlined,
-} from "@ant-design/icons";
-import { Link } from "react-router-dom";
-import firebase from "firebase";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
-import Search from "../forms/Search";
+} from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import firebase from 'firebase/compat/app'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import Search from '../forms/Search'
 
-const { SubMenu, Item } = Menu;
+const { SubMenu, Item } = Menu
 
 const Header = () => {
-  const [current, setCurrent] = useState("home");
+  const [current, setCurrent] = useState('home')
 
-  let dispatch = useDispatch();
-  let { user, cart } = useSelector((state) => ({ ...state }));
+  let dispatch = useDispatch()
+  let { user, cart } = useSelector((state) => ({ ...state }))
 
-  let history = useHistory();
+  let history = useHistory()
 
   const handleClick = (e) => {
     // console.log(e.key);
-    setCurrent(e.key);
-  };
+    setCurrent(e.key)
+  }
 
   const logout = () => {
-    firebase.auth().signOut();
+    firebase.auth().signOut()
     dispatch({
-      type: "LOGOUT",
+      type: 'LOGOUT',
       payload: null,
-    });
-    history.push("/login");
-  };
+    })
+    history.push('/login')
+  }
 
   return (
-    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-      <Item key="home" icon={<AppstoreOutlined />}>
-        <Link to="/">Home</Link>
+    <Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
+      <Item key='home' icon={<AppstoreOutlined />}>
+        <Link to='/'>Home</Link>
       </Item>
 
-      <Item key="shop" icon={<ShoppingOutlined />}>
-        <Link to="/shop">Shop</Link>
+      <Item key='shop' icon={<ShoppingOutlined />}>
+        <Link to='/shop'>Shop</Link>
       </Item>
 
-      <Item key="cart" icon={<ShoppingCartOutlined />}>
-        <Link to="/cart">
+      <Item key='cart' icon={<ShoppingCartOutlined />}>
+        <Link to='/cart'>
           <Badge count={cart.length} offset={[9, 0]}>
             Cart
           </Badge>
@@ -58,32 +58,32 @@ const Header = () => {
       </Item>
 
       {!user && (
-        <Item key="register" icon={<UserAddOutlined />} className="float-right">
-          <Link to="/register">Register</Link>
+        <Item key='register' icon={<UserAddOutlined />} className='float-right'>
+          <Link to='/register'>Register</Link>
         </Item>
       )}
 
       {!user && (
-        <Item key="login" icon={<UserOutlined />} className="float-right">
-          <Link to="/login">Login</Link>
+        <Item key='login' icon={<UserOutlined />} className='float-right'>
+          <Link to='/login'>Login</Link>
         </Item>
       )}
 
       {user && (
         <SubMenu
           icon={<SettingOutlined />}
-          title={user.email && user.email.split("@")[0]}
-          className="float-right"
+          title={user.email && user.email.split('@')[0]}
+          className='float-right'
         >
-          {user && user.role === "subscriber" && (
+          {user && user.role === 'subscriber' && (
             <Item>
-              <Link to="/user/history">Dashboard</Link>
+              <Link to='/user/history'>Dashboard</Link>
             </Item>
           )}
 
-          {user && user.role === "admin" && (
+          {user && user.role === 'admin' && (
             <Item>
-              <Link to="/admin/dashboard">Dashboard</Link>
+              <Link to='/admin/dashboard'>Dashboard</Link>
             </Item>
           )}
 
@@ -93,11 +93,11 @@ const Header = () => {
         </SubMenu>
       )}
 
-      <span className="float-right p-1">
+      <span className='float-right p-1'>
         <Search />
       </span>
     </Menu>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,63 +1,63 @@
-import React, { useState, useEffect } from "react";
-import { auth } from "../../firebase";
-import { toast } from "react-toastify";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from 'react'
+
+import { getAuth, sendSignInLinkToEmail } from 'firebase/auth'
+
+import { toast } from 'react-toastify'
+import { useSelector } from 'react-redux'
 
 const Register = ({ history }) => {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('')
 
-  const { user } = useSelector((state) => ({ ...state }));
+  const { user } = useSelector((state) => ({ ...state }))
 
   useEffect(() => {
-    if (user && user.token) history.push("/");
-  }, [user, history]);
+    if (user && user.token) history.push('/')
+  }, [user, history])
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    // console.log("ENV --->", process.env.REACT_APP_REGISTER_REDIRECT_URL);
+    e.preventDefault()
     const config = {
       url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
       handleCodeInApp: true,
-    };
+    }
 
-    await auth.sendSignInLinkToEmail(email, config);
+    const auth = getAuth()
+    await sendSignInLinkToEmail(auth, email, config)
     toast.success(
-      `Email is sent to ${email}. Click the link to complete your registration.`
-    );
-    // save user email in local storage
-    window.localStorage.setItem("emailForRegistration", email);
-    // clear state
-    setEmail("");
-  };
+      `Email is sent to ${email}. Click the link to complete your registration.`,
+    )
+    window.localStorage.setItem('emailForRegistration', email)
+    setEmail('')
+  }
 
   const registerForm = () => (
     <form onSubmit={handleSubmit}>
       <input
-        type="email"
-        className="form-control"
+        type='email'
+        className='form-control'
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        placeholder="Your email"
+        placeholder='Your email'
         autoFocus
       />
 
       <br />
-      <button type="submit" className="btn btn-raised">
+      <button type='submit' className='btn btn-raised'>
         Register
       </button>
     </form>
-  );
+  )
 
   return (
-    <div className="container p-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
+    <div className='container p-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
           <h4>Register</h4>
           {registerForm()}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
